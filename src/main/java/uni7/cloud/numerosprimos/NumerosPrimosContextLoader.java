984f32694c0362b938c7d2efd28da59d9a5cb725
Roberto Costa Lima Jr <roberto@enovar.com.br>
2019-04-26 10:39:08 -0300

    fix(): Remove code from context loader to stop calculating numbers on server app

--- src/main/java/uni7/cloud/numerosprimos/NumerosPrimosContextLoader.java
+++ src/main/java/uni7/cloud/numerosprimos/NumerosPrimosContextLoader.java
@@ -17,29 +17,6 @@ public class NumerosPrimosContextLoader implements ApplicationListener<ContextRe
     @Override
     public void onApplicationEvent(final ContextRefreshedEvent event) {
 
-        NumeroPrimoFinderThread n = new NumeroPrimoFinderThread();
-        Thread t = new Thread(n);
-        Thread t2 = new Thread(n);
-        Thread t3 = new Thread(n);
-        Thread t4 = new Thread(n);
-        Thread t5 = new Thread(n);
-        Thread t6 = new Thread(n);
-        Thread t7 = new Thread(n);
-        Thread t8 = new Thread(n);
-        Thread t9 = new Thread(n);
-        Thread t10 = new Thread(n);
-
-        t.start();
-        t2.start();
-        t3.start();
-        t4.start();
-        t5.start();
-        t6.start();
-        t7.start();
-        t8.start();
-        t9.start();
-        t10.start();
-
     }
 
 }
